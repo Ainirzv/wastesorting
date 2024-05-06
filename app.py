0836342af227c4bf08060f4fff225c9e934ff707
Aini Rizvi <114212852+Ainirzv@users.noreply.github.com>
2024-05-06 22:59:49 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import cv2
+from keras.models import load_model
+from keras.applications import imagenet_utils
+from keras.preprocessing.image import img_to_array
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+import time
+
+# Load model
+model = load_model(r'C:\Users\HP\Desktop\model_5class_resnet_87%.h5')
+
+labels = ['G&M', 'Organic', 'Other', 'Paper', 'Plastic']
+le = LabelEncoder()
+labels_encoded = le.fit_transform(labels)
+
+def preprocess_image(image):
+    resized_img = cv2.resize(image, (224, 224))
+    preprocessed_img = imagenet_utils.preprocess_input(resized_img)
+    preprocessed_img = img_to_array(preprocessed_img)
+    preprocessed_img = np.expand_dims(preprocessed_img / 255, 0)
+    return preprocessed_img
+
+def format_array_to_str(array):
+    return str(array.tolist())[1:-1]
+
+def main():
+    st.title('Real-Time Object Classification')
+    st.sidebar.title('Settings')
+    
+    cap = cv2.VideoCapture(0)
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 1
+    color = (0, 255, 0)
+    thickness = 2
+    
+    while True:
+        start_time = time.time()
+        success, img = cap.read()
+        if not success:
+            st.error("Failed to capture frame from webcam.")
+            break
+
+        preprocessed_img = preprocess_image(img)
+        predictions = model.predict(preprocessed_img)
+        confidence = np.max(predictions)
+        predicted_class_index = np.argmax(predictions)
+
+        fps = 1.0 / (time.time() - start_time)
+        st.write('FPS:', fps)
+
+        if confidence > 0.85:
+            predicted_class = labels[predicted_class_index]
+            cv2.putText(img, predicted_class, (50, 50), font, font_scale, color, thickness, cv2.LINE_AA)
+            cv2.putText(img, format_array_to_str(confidence), (50, 150), font, font_scale, color, thickness, cv2.LINE_AA)
+            st.image(img, channels="BGR")
+        
+        if st.button('Stop'):
+            break
+
+if __name__ == '__main__':
+    main()
